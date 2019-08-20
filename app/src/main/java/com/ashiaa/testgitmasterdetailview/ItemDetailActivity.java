3b82ed89d7dcf57494e51830b24d633afb209429
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-20 22:03:06 +0200

    add branch1

--- app/src/main/java/com/ashiaa/testgitmasterdetailview/ItemDetailActivity.java
+++ app/src/main/java/com/ashiaa/testgitmasterdetailview/ItemDetailActivity.java
@@ -23,6 +23,8 @@ import android.view.MenuItem;
  */
 public class ItemDetailActivity extends AppCompatActivity {
 
+    //this is branch1
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
